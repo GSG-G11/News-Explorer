1b5aa2d57dd329a870f798a03f8397cf5c7e7246
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-02-09 15:58:32 +0200

    implement News API code Realtes #14

--- js/dom.js
+++ js/dom.js
@@ -11,19 +11,86 @@ const wordAudio = getElement("audio");
 const wordExamples = getElement("example");
 const wordSynonyms = getElement("synonyms");
 
+const newsContainer = document.querySelector(".news-container")
 const dictionaryContainer = document.querySelector(".dictionary-container");
 
 const wordUrl = (word) => `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-const checkLength = (parent, yourLength, operator) => {
-    if (operator === 'grater than') {
-        return parent.children.length > yourLength;
-    } else if (operator === 'less than') {
-        return parent.children.length < yourLength;
-    } else if (operator === 'equal') {
-        return parent.children.length === yourLength;
-    }
+const checkLength = (parent, yourLength) => {
+
+    return parent.children.length === yourLength;
 }
+
+const newsUrl = (report) => `https://newsapi.org/v2/everything?q=${report}&from=2022-01-09&sortBy=publishedAt&apiKey=d4316dafee0b473796d7807eeb993f65`;
+
+
+
+searchNewsIcon.addEventListener("click", () => {
+    const subject = searchNewsInput.value;
+    fetch(newsUrl(subject), (data) => {
+        const articles = data.articles.slice(0, 6);
+        if (checkLength(newsContainer, 6)) {
+            newsContainer.textContent = '';
+            articles.forEach((ele, index) => {
+                const card = document.createElement("div");
+                card.className = "news-card"
+                const title = document.createElement("a");
+                title.className = "news-title"
+                const article = document.createElement("p");
+                article.className = "news-article"
+                const image = document.createElement("img");
+                image.className = "news-img"
+
+                const icon = document.createElement("i")
+                icon.className = "fas fa-map-marked-alt"
+                const puplisher = document.createElement("p");ّ
+                puplisher.className = "news-puplisher"
+                card.appendChild(title);
+                card.appendChild(article);
+                card.appendChild(image);
+                card.appendChild(icon);
+                card.appendChild(puplisher);
+                newsContainer.appendChild(card)
+
+                title.textContent = data.articles[index].title;
+                title.setAttribute("target", "_blank")
+                title.setAttribute("href", data.articles[index].url)
+                article.textContent = data.articles[index].content;
+                image.setAttribute("src", data.articles[index].urlToImage);
+                puplisher.textContent = data.articles[index].author;
+            })
+        } else if (checkLength(newsContainer, 0)) {
+            articles.forEach((ele, index) => {
+                const card = document.createElement("div");
+                card.className = "news-card"
+                const title = document.createElement("a");
+                title.className = "news-title"
+                const article = document.createElement("p");
+                article.className = "news-article"
+                const image = document.createElement("img");
+                image.className = "news-img"
+
+                const icon = document.createElement("i")
+                icon.className = "fas fa-map-marked-alt"
+                const puplisher = document.createElement("p");
+                puplisher.className = "news-puplisher"
+                card.appendChild(title);
+                card.appendChild(article);
+                card.appendChild(image);
+                card.appendChild(icon);
+                card.appendChild(puplisher);
+                newsContainer.appendChild(card)
+
+                title.textContent = data.articles[index].title;
+                title.setAttribute("target", "_blank")
+                title.setAttribute("href", data.articles[index].url)
+                article.textContent = data.articles[index].content;
+                image.setAttribute("src", data.articles[index].urlToImage);
+                puplisher.textContent = data.articles[index].author;
+            })
+        }
+    })
+})
 
 searchDictionaryIcon.addEventListener("click", () => {
     const word = searchDictionaryInput.value;
@@ -44,7 +111,7 @@ searchDictionaryIcon.addEventListener("click", () => {
         } else {
             wordExamples.textContent = data[0].meanings[0].definitions[0].example;
         }
-        if (checkLength(dictionaryContainer, 5, 'equal')) {
+        if (checkLength(dictionaryContainer, 5)) {
             dictionaryContainer.removeChild(dictionaryContainer.lastChild);
         }
     }, () => {
@@ -52,11 +119,11 @@ searchDictionaryIcon.addEventListener("click", () => {
         wordAudio.src = `No Data`;
         wordSynonyms.textContent = `No Data`;
         wordExamples.textContent = `No Data`;
-        if (checkLength(dictionaryContainer, 5, 'equal')) {
+        if (checkLength(dictionaryContainer, 5)) {
             dictionaryContainer.removeChild(dictionaryContainer.lastChild);
         }
     }, () => {
-        if (checkLength(dictionaryContainer, 4, 'equal')) {
+        if (checkLength(dictionaryContainer, 4)) {
             let ele = document.createElement('h2');
             ele.textContent = "Server Error";
             dictionaryContainer.appendChild(ele);
